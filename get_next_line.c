7bc0fb6d9c91a98fd6201d5d77e93bbe68150451
Jenna Mustajärvi <jemustaj@student.42porto.com>
2025-02-12 22:25:11 +0000

    started gnl from scratch

--- get_next_line.c
+++ get_next_line.c
@@ -5,54 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jemustaj <jemustaj@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/09 19:27:39 by jemustaj          #+#    #+#             */
-/*   Updated: 2025/02/12 16:03:49 by jemustaj         ###   ########.fr       */
+/*   Created: 2025/02/12 21:00:57 by jemustaj          #+#    #+#             */
+/*   Updated: 2025/02/12 21:39:38 by jemustaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-char	*read_to_stash(int fd, char *stash);
+char	*read_to_stash(char *stash);
 char	*extract_line(char *stash);
-char	*update_stash(char *stash, char *line);
 
 char	*get_next_line(int fd)
 {
-	char		*line;
-	static char	*stash;
+	char	*stash;
+	char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	stash = read_to_stash(fd, stash);
-	if (!stash)
-		return (NULL);
+	stash = read_to_stash(stash);
 	line = extract_line(stash);
-	if (!line || line[0] == '\0')
-	{
-		if (line[0] == '\0')
-			return (free(stash), free(line), NULL);
-		return (NULL);
-	}
-	stash = update_stash(stash, line);
-	if (!stash)
-		line[ft_strlen(line) - 1] = '\0';
-	return (line);
 }
 
-char	*read_to_stash(int fd, char *stash)
+char	*read_to_stash(char *stash)
 {
-	ssize_t	readed;
 	char	*buff;
 	char	*temp;
+	ssize_t	readed;
 
-	if (stash == NULL)
+	if (!stash)
 		stash = ft_strdup("");
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
-		return (NULL);
-	readed = 1;
-	while (!find_newline(stash) && readed > 0)
+		return (free(stash), NULL);
+	while (readed > 0 && !find_newline(stash))
 	{
 		readed = read(fd, buff, BUFFER_SIZE);
 		if (readed == -1)
@@ -70,44 +55,11 @@ char	*read_to_stash(int fd, char *stash)
 
 char	*extract_line(char *stash)
 {
-	char	*next_line;
+	char	*line;
 	size_t	line_len;
-	size_t	i;
-
-	line_len = ft_strclen(stash, '\n') + 1;
-	next_line = malloc(sizeof(char) * (line_len + 1));
-	if (!next_line)
-		return (NULL);
-	i = 0;
-	while (stash[i] && stash[i] != '\n')
-	{
-		next_line[i] = stash[i];
-		i++;
-	}
-	if (stash[i] == '\n')
-		next_line[i++] = '\n';
-	next_line[i] = '\0';
-	return (next_line);
-}
-
-char	*update_stash(char *stash, char *line)
-{
-	char	*next_stash;
-	size_t	line_len;
-	size_t	i;
-	size_t	j;
 
 	if (!stash)
-		return (NULL);
-	line_len = ft_strlen(line);
-	i = ft_strlen(stash + line_len);
-	next_stash = malloc(sizeof(char) * (i + 1));
-	if (!next_stash)
-		return (free(stash), NULL);
-	j = 0;
-	while (stash[line_len])
-		next_stash[j++] = stash[line_len++];
-	next_stash[j] = '\0';
-	free(stash);
-	return (next_stash);
+		return (); // NULL ? and check also !*stash?
+	line_len = ft_strclen(stash, '\n'); //also search for '\0' inside function
+	line = malloc(sizeof(char) * (ft_strlen(stash) - line_len + 2));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,73 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jemustaj <jemustaj@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/09 20:50:23 by jemustaj          #+#    #+#             */
-/*   Updated: 2025/02/12 15:23:20 by jemustaj         ###   ########.fr       */
+/*   Created: 2025/02/12 21:26:50 by jemustaj          #+#    #+#             */
+/*   Updated: 2025/02/12 22:24:15 by jemustaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	find_newline(char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0); //maybe NULL ?
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	if (str[i] == '\0')
+		return (1); //return 1 or NULL ?
+	return (0);
+}
 
 size_t	ft_strlen(char *str)
 {
 	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strdup(char *str)
-{
-	int		i;
-	char	*dest;
-
-	if (!str)
-		return (NULL);
-	dest = malloc(sizeof(char) * (ft_strlen(str) + 1));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		dest[i] = str[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strjoin(const char *str1, const char *str2)
-{
-	char	*dest;
-	int		i;
-
-	if (!str1 && !str2)
-		return (NULL);
-	else if (!str1)
-		return ((char *)(str2));
-	else if (!str2)
-		return ((char *)(str1));
-	dest = malloc(sizeof(char) * (ft_strlen((char *)str1)
-				+ ft_strlen((char *)str2) + 1));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (*str1)
-		dest[i++] = *str1++;
-	while (*str2)
-		dest[i++] = *str2++;
-	dest[i] = '\0';
-	return (dest);
-}
-
-size_t	ft_strclen(char	*str, char c)
+size_t	ft_strclen(char *str, char c)
 {
 	size_t	i;
 
-	if (!str || !c)
-		return (0);
+	if (!stash || !c)
+		return (NULL);
 	i = 0;
 	while (str[i])
 	{
@@ -82,18 +58,13 @@ size_t	ft_strclen(char	*str, char c)
 	return (i);
 }
 
-int	find_newline(char *str)
+char	*ft_strdup(char *str)
 {
+	char	*dest;
+	size_t	str_len;
 	size_t	i;
+	size_t	j;
 
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			return (1);
-		i++;
-	}
-	return (0);
+	str_len = ft_strlen(str);
+	dest = malloc(sizeof(char) * (str_len + 1));
 }
